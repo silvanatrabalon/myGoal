251a49d89bb0af8080bd4c006c724166f2e40327
sil <silvana.trabalon@gmail.com>
2020-07-27 10:19:38 -0300

    add components and reach the api

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,18 @@ import "bootswatch/dist/slate/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import TaskList  from "./components/TaskList";
 import TaskForm from "./components/TaskForm";
+import Navigation from "./components/Navbar";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={TaskList} />
-        <Route exact path="/new-task" component={TaskForm} />
-      </Switch>
+      <Navigation/>
+      <div className="container p4">
+        <Switch>
+          <Route exact path="/" component={TaskList} />
+          <Route exact path="/new-task" component={TaskForm} />
+        </Switch>
+      </div>
     </Router>
 
   );
