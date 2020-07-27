6391cacef6ddca600bf6f5d8af272400ea3f4ddb
sil <silvana.trabalon@gmail.com>
2020-07-27 11:59:21 -0300

    add schema and mutation for remove

--- backend/src/graphql/resolvers/Mutation.js
+++ backend/src/graphql/resolvers/Mutation.js
@@ -5,6 +5,9 @@ const Mutation = {
   createTask: async (_, {title, content, author}) => { //ignore parent with _
     const newTask = new Task({title, content, author});
     return await newTask.save() //all the operations on bd are async, the return is the new task
+  },
+  removeTask: async (_,  {_id}) => { //ignore parent with _
+    return await Task.findByIdAndDelete(_id)  //delete from mongo db
   }
 };
 
--- backend/src/graphql/resolvers/Query.js
+++ backend/src/graphql/resolvers/Query.js
@@ -8,7 +8,6 @@ const Query = {
   task: async () => {
     return await Task.find()
   }
-
 }
 
 export default Query;
