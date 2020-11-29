bc95c5313dcf439d2f35328027405e7405996fef
sil <silvana.trabalon@gmail.com>
2020-11-29 12:57:09 -0300

    fixing schema for removeTask mutation

--- backend/src/graphql/resolvers/Mutation.js
+++ backend/src/graphql/resolvers/Mutation.js
@@ -3,11 +3,11 @@ import Task from '../../models/Task'
 
 const Mutation = {
   //the params are the fields of the Task schema
-  createTask: async (_, {title, content, author}) => { //ignore fuction and parents with _
+  createTask: async (_, {title, content, author}) => { //ignore function and parents with _
     const newTask = new Task({title, content, author});
     return await newTask.save() //all the operations on bd are async, the return is the new task
   },
-  removeTask: async (_,  {_id}) => { //ignore parent with _
+  removeTask: async (_, {_id}) => { //ignore parent with _
     return await Task.findByIdAndDelete(_id)  //delete from mongo db
   }
 };
